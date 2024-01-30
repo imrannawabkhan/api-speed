559640838841550f6ecdaac8efa8b5c0e0ab1ada
imrannawabkhan <42738356+imrannawabkhan@users.noreply.github.com>
2024-01-30 14:08:50 +0400

    Add files via upload

---
+++ webapi-aspnet/webapi-asp.net/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webapi.Controllers
+{
+    [ApiController]
+    [Route("api/hello")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var message = "Hello World! From Asp.net";
+            return Ok(new { Message = message });
+        }
+    }
+}
